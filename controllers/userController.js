86148e87148da02b225a0aaa172c3745c56f6dfb
euxhen <euxhenibi@hotmail.com>
2020-09-22 15:16:46 +0200

    chan

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,36 +2,38 @@ const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const multer = require("multer");
+const sharp = require("sharp");
 
-const multerStorage = multer.memoryStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images/user-images");
-  },
-  filename: (req, file, cb) => {
-    const ext = file.mimetype.split("/")[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image")) {
     cb(null, true);
   } else {
-    cb(
-      new AppError(
-        "File nuk eshte nje imazh! Ju lutem ngarkoni nje imazh",
-        400
-      ),
-      false
-    );
+    cb(new AppError("Not an image! Please upload only images.", 400), false);
   }
 };
+
 const upload = multer({
   storage: multerStorage,
   fileFilter: multerFilter,
 });
 
 exports.uploadUserPhoto = upload.single("photo");
+
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/images/user-images/${req.file.filename}`);
+
+  next();
+});
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
