1d05ea51d163fc17849641a563f2caa8fb81df0b
euxhen <euxhenibi@hotmail.com>
2020-09-28 23:21:15 +0200

    change

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: SECRET_ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
 });
 const uploadS3 = multer({
   storage: multerS3({
