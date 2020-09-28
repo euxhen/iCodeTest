b86a6afbf97181327ed91325513e8229911d86b4
euxhen <euxhenibi@hotmail.com>
2020-09-28 23:35:57 +0200

    change

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,10 +57,10 @@ exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  await sharp(req.file.buffer)
-    .resize(500, 500)
-    .toFormat("jpeg")
-    .jpeg({ quality: 90 });
+  // await sharp(req.file.buffer)
+  //   .resize(500, 500)
+  //   .toFormat("jpeg")
+  //   .jpeg({ quality: 90 });
   // .toFile(`https://res.cloudinary.com/devztowmv/image/upload`);
 
   next();
